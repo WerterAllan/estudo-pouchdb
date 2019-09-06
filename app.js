e65bd66228b475a6a7d7af87af34adc8a57340ca
Werter <werter@hotmail.com.br>
2019-09-06 16:23:41 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+import './pouchdb-7.1.1.js';
+import TabelaDeTarefas from './tabela-de-tarefas.js'
+import Formulario from './formulario.js';
+import TodoApp from './todo-app.js';
+
+const app = async () => {
+
+    let tabela = TabelaDeTarefas([], "tarefas");
+    let todoApp = TodoApp("todos");
+
+
+
+    document.getElementById("btnAdicionarTarefas")
+        .addEventListener("click", x => {
+
+            const todo = Formulario.Add();
+            tabela.Add(todo);
+            todoApp.AddTodo(todo);
+
+        });
+
+
+    document.getElementById("btnDestruir")
+        .addEventListener("click", x => todoApp.DestruirBanco() );
+
+}
+
+
+
+window.addEventListener('load', app);
